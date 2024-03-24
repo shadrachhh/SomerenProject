dd34bf2bab9906659aa867ffae7701cd01e676e0
ELCHUPACABRA01 <144237366+ELCHUPACABRA01@users.noreply.github.com>
2024-03-24 14:13:43 +0100

    Delete SomerenModel directory

--- SomerenModel/Teacher.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SomerenModel
-{
-    public class Teacher
-    {
-        public int Id { get; set; }     // database id
-        public string Name { get; set; }
-        public int Number { get; set; } // LecturerNumber, e.g. 47198
-    }
-}
